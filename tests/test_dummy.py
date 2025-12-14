9593e114d52ebcdf2948e1be9b2a7cfed7068c26
yhong123 <yean-hoon.ong@ucl.ac.uk>
2025-12-14 19:21:49 +0000

    Update test module

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,57 +1,41 @@
 """An example set of tests."""
 
-import unittest
-import math
-import torch
+
 from gppro.models import GPGlobal
 from gppro.models import GPPro
+from gppro.data import generate_synthetic_data_1
 from sklearn.metrics import mean_absolute_error
 
-# Batch training test: Let's learn hyperparameters on a sine dataset, but test on a sine dataset and a cosine dataset
-# in parallel.
-train_x1 = torch.linspace(0, 2, 11).unsqueeze(-1)
-train_y1 = torch.sin(train_x1 * (2 * math.pi)).squeeze()
-train_y1.add_(torch.randn_like(train_y1).mul_(0.01))
-test_x1 = torch.linspace(0, 2, 51).unsqueeze(-1)
-test_y1 = torch.sin(test_x1 * (2 * math.pi)).squeeze()
 
 
-class TestGPGlobal(unittest.TestCase):
+def test_gpglobal_train_predict() -> None:
+    """Test is merely a placeholder."""
+    train_x1, train_y1, test_x1, test_y1 = generate_synthetic_data_1()
+    gp = GPGlobal()
+    gp.train(train_x1, train_y1) 
+
+    test_mean, test_var = gp.predict(test_x1)
     
-    def test_train_predict(self) -> None:
-        """Test is merely a placeholder."""
-        gp = GPGlobal()
-        gp.train(train_x1, train_y1) 
-    
-        test_mean, test_var = gp.predict(test_x1)
-        
-        preds1 = test_mean
-        mae = mean_absolute_error(test_y1, preds1)
-        self.assertLess(mae, 0.1)
-        #mean_abs_error1 = torch.mean(torch.abs(test_y1 - preds1))
-        #self.assertLess(mean_abs_error1.squeeze().item(), 0.1)
+    preds1 = test_mean
+    mae = mean_absolute_error(test_y1, preds1)
+    assert mae < 0.1, f"MAE too large: {mae}"
     
     
-class TestGPPro(unittest.TestCase):
+def test_gppro_train_predict() -> None:
+    """Test is merely a placeholder."""
+    train_x1, train_y1, test_x1, test_y1 = generate_synthetic_data_1()
+    gp_pro = GPPro()
+    gp_pro.train(train_x1, train_y1) 
+
+    test_mean, test_var = gp_pro.predict(test_x1)
     
-    def test_train_predict(self) -> None:
-        """Test is merely a placeholder."""
-        gp_pro = GPPro()
-        gp_pro.train(train_x1, train_y1) 
+    preds1 = test_mean
+    mae = mean_absolute_error(test_y1, preds1)
+    assert mae < 0.1, f"MAE too large: {mae}"
     
-        test_mean, test_var = gp_pro.predict(test_x1)
-        
-        preds1 = test_mean
-        mae = mean_absolute_error(test_y1, preds1)
-        self.assertLess(mae, 0.1)
-        #mean_abs_error1 = torch.mean(torch.abs(test_y1 - preds1))
-        #self.assertLess(mean_abs_error1.squeeze().item(), 0.1)
 
 
 def test_stupid_example() -> None:
     """Test is merely a placeholder."""
     assert True
 
-
-if __name__ == '__main__':
-    unittest.main()
